6bb1bf52c31036c36bc585df07c36a25c0a67955
Jarr0ds <97823325+Jarr0ds@users.noreply.github.com>
2026-06-01 00:41:47 +1000

    Added a more phone friendly button to the website.

---
+++ js/button.js
@@ -0,0 +1,11 @@
+document.addEventListener("click", (event) => {
+    if (event.target.closest("#navbar-hamburger-button")) {
+        const targetDiv = document.querySelector(".hamburger-nav-links");
+
+        if (targetDiv.style.display === "flex") {
+            targetDiv.style.display = "none";
+        } else {
+            targetDiv.style.display = "flex";
+        }
+    }
+});
