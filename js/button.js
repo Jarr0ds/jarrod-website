811b8900e04f183aca92d9bbce2a8069f8b5d162
Jarr0ds <97823325+Jarr0ds@users.noreply.github.com>
2026-06-07 01:30:45 +1000

    changed colour scheme, added a new readablility skip to main option index.html is mostly completed and work has started on resume

--- js/button.js
+++ js/button.js
@@ -1,6 +1,6 @@
 document.addEventListener("click", (event) => {
-    if (event.target.closest("#navbar-hamburger-button")) {
-        const targetDiv = document.querySelector(".hamburger-nav-links");
+    if (event.target.closest("#site-nav-toggle")) {
+        const targetDiv = document.querySelector(".site-nav-list");
 
         if (targetDiv.style.display === "flex") {
             targetDiv.style.display = "none";
